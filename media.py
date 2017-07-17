a2310b79a972c5268bd41b43d6d5ba1e83f721d4
alex <zhurbitsky@gmail.com>
2017-07-17 18:05:10 +0300

    Fix PEP 8

--- media.py
+++ media.py
@@ -1,7 +1,17 @@
 
 class Movie():
     """Simple movie info storage"""
+
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """
+        Initialize instance of class Movie
+
+        :param movie_title: string
+        :param movie_storyline: string
+        :param poster_image: string
+        :param trailer_youtube: string
+        """
+        
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
